8414b1eff2a940727553af876322ba755569acde
Gallerani <sgallerani@frinx.io>
2019-07-22 13:00:31 +0200

    Fix also all.sh

--- topologies/all.sh
+++ topologies/all.sh
@@ -1,5 +1,5 @@
-python ../mockdevice.py 0.0.0.0 10000 10001 ssh ../devices/cisco_IOS.json &
-python ../mockdevice.py 0.0.0.0 10001 10002 ssh ../devices/cisco_XR.json &
-python ../mockdevice.py 0.0.0.0 10002 10003 ssh ../devices/huawei_VRP.json &
+python ../mockdevice.py 0.0.0.0 10000 1 ssh ../devices/cisco_IOS.json &
+python ../mockdevice.py 0.0.0.0 10001 1 ssh ../devices/cisco_XR.json &
+python ../mockdevice.py 0.0.0.0 10002 1 ssh ../devices/huawei_VRP.json &
 
 wait
