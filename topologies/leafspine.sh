c72a9f21ff7076fcee87bdddd621cc4fb2d7882d
Ladislav Moravek <lmoravek@frinx.io>
2019-07-23 09:11:01 +0200

    updated calls to correspond to the new inteface

--- topologies/leafspine.sh
+++ topologies/leafspine.sh
@@ -1,9 +1,9 @@
-python ../mockdevice.py 0.0.0.0 11000 11001 ssh ../devices/leafspine/leaf1.json &
-python ../mockdevice.py 0.0.0.0 11001 11002 ssh ../devices/leafspine/leaf2.json &
-python ../mockdevice.py 0.0.0.0 11002 11003 ssh ../devices/leafspine/leaf3.json &
-python ../mockdevice.py 0.0.0.0 11003 11004 ssh ../devices/leafspine/leaf4.json &
-python ../mockdevice.py 0.0.0.0 11004 11005 ssh ../devices/leafspine/leaf5.json &
-python ../mockdevice.py 0.0.0.0 12000 12001 ssh ../devices/leafspine/spine1.json &
-python ../mockdevice.py 0.0.0.0 12001 12002 ssh ../devices/leafspine/spine2.json &
+python ../mockdevice.py 0.0.0.0 11000 1 ssh ../devices/leafspine/leaf1.json &
+python ../mockdevice.py 0.0.0.0 11001 1 ssh ../devices/leafspine/leaf2.json &
+python ../mockdevice.py 0.0.0.0 11002 1 ssh ../devices/leafspine/leaf3.json &
+python ../mockdevice.py 0.0.0.0 11003 1 ssh ../devices/leafspine/leaf4.json &
+python ../mockdevice.py 0.0.0.0 11004 1 ssh ../devices/leafspine/leaf5.json &
+python ../mockdevice.py 0.0.0.0 12000 1 ssh ../devices/leafspine/spine1.json &
+python ../mockdevice.py 0.0.0.0 12001 1 ssh ../devices/leafspine/spine2.json &
 
 wait
--- topologies/link_aggregation.sh
+++ topologies/link_aggregation.sh
@@ -1,4 +1,4 @@
-python ../mockdevice.py 0.0.0.0 13001 13002 ssh ../devices/cisco_XR.json &
-python ../mockdevice.py 0.0.0.0 13002 13003 ssh ../devices/cisco_XR.json &
+python ../mockdevice.py 0.0.0.0 13001 1 ssh ../devices/cisco_XR.json &
+python ../mockdevice.py 0.0.0.0 13002 1 ssh ../devices/cisco_XR.json &
 
 wait
