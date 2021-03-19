61c4e01ab364a5178b353fa1529c6ba203c86fe5
Kristina Nevolnikova <knevolnikova@frinx.io>
2021-03-19 14:31:42 +0100

    VZ-418: add new devices, XR5, XR623, XR661, XR7, junos.
    Signed-off-by: Kristina Nevolnikova <knevolnikova@frinx.io>

--- topologies/all.sh
+++ topologies/all.sh
@@ -1,5 +1,10 @@
 python ../mockdevice.py 0.0.0.0 10000 1 ssh ../devices/cisco_IOS.json &
 python ../mockdevice.py 0.0.0.0 10001 1 ssh ../devices/cisco_XR.json &
 python ../mockdevice.py 0.0.0.0 10002 1 ssh ../devices/huawei_VRP.json &
+python ../mockdevice.py 0.0.0.0 10003 1 ssh ../devices/cisco_XR5.json &
+python ../mockdevice.py 0.0.0.0 10004 1 ssh ../devices/cisco_XR623.json &
+python ../mockdevice.py 0.0.0.0 10005 1 ssh ../devices/cisco_XR661.json &
+python ../mockdevice.py 0.0.0.0 10006 1 ssh ../devices/cisco_XR701.json &
+python ../mockdevice.py 0.0.0.0 10007 1 ssh ../devices/junos_14.json &
 
 wait
